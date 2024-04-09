3942b5e0c7a4f055d97ff1134506bcd5779793c9
DR1FT3R01 <drifterro11@outlook.com>
2024-04-09 14:40:33 +0200

    xd

--- Kod/Player.cs
+++ Kod/Player.cs
@@ -1,4 +1,25 @@
 class Player
 {
-    
+
+
+
+
+
+
+
+
+
+
+   public void Move()
+    {
+        ConsoleKeyInfo pressedKey = Console.ReadKey(true);
+        if (pressedKey.Key == ConsoleKey.A)
+        {
+            X -= 1;
+        }
+        else if(pressedKey.Key == ConsoleKey.D)
+        {
+            X += 1;
+        }
+    }
 }
