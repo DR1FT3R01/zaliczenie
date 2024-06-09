5a52c06b91cfbd3fb23d0d2df27d0781e30f9e4f
Saexane <157719912+Saexane@users.noreply.github.com>
2024-06-09 17:57:13 +0200

    Adding Game Console(kinda)

---
+++ Kod/GameConsole.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zaliczenie.Kod
+{
+    public class GameConsole
+    {
+        private List<string> messages = new List<string>();
+        private int consoleWidth;
+        private int consoleHeight;
+
+        public GameConsole(int width, int height)
+        {
+            consoleWidth = width;
+            consoleHeight = height;
+            AddMessage("hi");
+        }
+
+        public void AddMessage(string message)
+        {
+            messages.Add(message);
+            if (messages.Count > consoleHeight)
+            {
+                messages.RemoveAt(0);
+            }
+        }
+
+        public void DisplayMessages()
+        {
+            Console.SetCursorPosition(0, 0);
+            for (int i = 0; i < consoleHeight; i++)
+            {
+                if (i < messages.Count)
+                {
+                    Console.WriteLine(messages[i].PadRight(consoleWidth));
+                }
+                else
+                {
+                    Console.WriteLine(new string(' ', consoleWidth));
+                }
+            }
+        }
+    }
+
+}
