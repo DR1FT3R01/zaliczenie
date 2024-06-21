24bb3ce26f71c5f9075820fadbe570e63e32ff97
chmvrka <juliaxmajewska2@gmail.com>
2024-06-22 01:54:40 +0200

    MiniGame in progress

--- Kod/MiniGame.cs
+++ Kod/MiniGame.cs
@@ -1,24 +1,29 @@
 public class MiniGame
 {
-    //public static void Main(string[] args)
-    public void miniGame()
+    internal bool DidThePlayerWin()
     {
-        // Random random= new Random();
-        // int choose;
-        // int number;
+        Random rng = new Random();
+        int generatedNumber = rng.Next(1, 4);
 
-        // choose = int.Parse(());
-        // number = random.Next(1, 10);
+        Console.SetCursorPosition(0, 0);
+        Console.Write("                                                                          ");
+        Console.SetCursorPosition(0, 0);
+        Console.Write("Guess a number between 1 and 3:");
+        int.TryParse(Console.ReadLine()?.Trim(), out int guess);
 
-
-        // while (choose != number)
-        // {
-        //     Console.WriteLine("Choose your number");
-        //     if (choose > number)
-        //     {
-        //         Console.WriteLine("Hit");
-        //     }
-
-        // }
+        if (guess == generatedNumber)
+        {
+            Console.SetCursorPosition(0, 0);
+            Console.Write("You guessed right! Press Any key to continue...                       ");
+            Console.ReadKey(true);
+            return true;
+        }
+        else
+        {
+            Console.SetCursorPosition(0, 0);
+            Console.Write("You guessed wrong! Press Any key to continue...                       ");
+            Console.ReadKey(true);
+            return false;
+        }
     }
 }
