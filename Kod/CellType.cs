e8a772d22fc6fd383b90165561e51f0209123538
DR1FT3R01 <drifterro11@outlook.com>
2024-06-22 19:43:06 +0200

    added new map but it doesnt display
    probably because its too big

--- Kod/CellType.cs
+++ Kod/CellType.cs
@@ -1,6 +1,18 @@
 public enum CellType
 {
-    Wall = 1,
-    Floor = 2,
-    Empty = 3,
+    Wall = 2,
+    Floor = 4,
+    Empty = 1,
+    Rock = 3,
+    Corners = 5,
+    Grass = 6,
+    Enviro1 = 7,
+    Enviro2 = 8,
+    Torch = 9,
+    Web = 0,
+    Portal = 10,
+    Ring = 11,
+    Gold = 12,
+    Left = 13,
+    Right = 14,
 }
